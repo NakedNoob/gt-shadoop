2c0b9fc0310def1abd792d8462c224f7a20dd87e
ashearer23 <ashearer23@gmail.com>
2013-11-06 16:56:22 +0000

    added javadocs for classes

--- src/org/geotools/data/shadoop/query/Shadoop.java
+++ src/org/geotools/data/shadoop/query/Shadoop.java
@@ -3,17 +3,35 @@ package org.geotools.data.shadoop.query;
 import org.geotools.data.shadoop.query.ShadoopDS;
 import org.geotools.data.shadoop.ShadoopPluginConfig;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Shadoop.
+ */
 public class Shadoop {
 
+	/**
+	 * Instantiates a new shadoop.
+	 *
+	 * @param config the config
+	 */
 	public Shadoop(ShadoopPluginConfig config) {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Gets the ds.
+	 *
+	 * @param db the db
+	 * @return the ds
+	 */
 	public ShadoopDS getDS(String db) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Close.
+	 */
 	public void close() {
 		// TODO Auto-generated method stub
 		
